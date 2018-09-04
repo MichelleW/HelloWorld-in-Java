c648d47de209b1752dfdade1e9b4ca3660b7e71b
Michelle <michelle.wu177@gmail.com>
2018-09-04 10:47:23 -0700

    first java program

---
+++ MyName.java
@@ -0,0 +1,6 @@
+public class MyName {
+    public static void main(String[] args) {
+        System.out.println("My name is michelle I'm 100 years old, My hometown is San Francisco,ca");
+    }
+}
+ 
